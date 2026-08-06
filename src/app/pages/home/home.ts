f4c8cd81f5d35aad4506156942c18ee86bc26100
KamyCoding <kamyar.zamanfar@gmail.com>
2026-08-06 18:38:29 +0200

    feat(home): integrate projects section

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { About } from './sections/about/about';
 import { Hero } from './sections/hero/hero';
+import { Projects } from './sections/projects/projects';
 import { Skills } from './sections/skills/skills';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, About, Skills],
+  imports: [Hero, About, Skills, Projects],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
