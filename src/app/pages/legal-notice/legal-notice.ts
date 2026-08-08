49852d7d4b746104844c272a709ef3f400aa94a4
KamyCoding <kamyar.zamanfar@gmail.com>
2026-08-09 01:46:01 +0200

    feat(legal): implement responsive legal notice page

--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { Footer } from '../../layout/footer/footer';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [],
+  imports: [RouterLink, Footer],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LegalNotice {}
