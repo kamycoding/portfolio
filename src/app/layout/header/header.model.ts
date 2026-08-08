6e164d43cd480adba8ff778044c771b0889e6ee6
KamyCoding <kamyar.zamanfar@gmail.com>
2026-08-08 15:37:17 +0200

    refactor(social-links): share social link configuration

--- src/app/layout/header/header.model.ts
+++ src/app/layout/header/header.model.ts
@@ -3,11 +3,3 @@ export interface NavigationItem {
   readonly fragment: string;
   readonly hoverDecorationSrc: string;
 }
-
-export interface SocialLink {
-  readonly id: string;
-  readonly href: string;
-  readonly accessibleLabel: string;
-  readonly iconSrc: string;
-  readonly opensInNewTab: boolean;
-}
